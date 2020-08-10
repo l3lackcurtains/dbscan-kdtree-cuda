4c00dc024b86957a3ff53a4bdab1b05b279b11d6
Madhav Poudel <king.ov.universe@gmail.com>
2020-08-10 14:44:05 -0600

    kd tree optimized

--- kdtree/kdtree.cu
+++ kdtree/kdtree.cu
@@ -1,171 +1,6 @@
 #include "kdtree.h"
-#include<bits/stdc++.h> 
 
-kdTree::kdTree() {}
-kdTree::~kdTree() {}
-
-double kdTree::dist(struct kdNode *a, struct kdNode *b) {
-    double t, d = 0;
-    int dim = DIMENSION;
-    while (dim--) {
-        t = a->x[dim] - b->x[dim];
-        d += t * t;
-    }
-    return d;
-}
-
-void kdTree::swap(struct kdNode *x, struct kdNode *y) {
-    double tmp[DIMENSION];
-    int id;
-    memcpy(tmp, x->x, sizeof(tmp));
-    id = x->id;
-
-    memcpy(x->x, y->x, sizeof(tmp));
-    x->id = y->id;
-
-    memcpy(y->x, tmp, sizeof(tmp));
-    y->id = id;
-}
-struct kdNode *kdTree::findMedian(struct kdNode *start, struct kdNode *end,
-    int idx) {
-    if (end <= start) return NULL;
-    if (end == start + 1) return start;
-
-    struct kdNode *p, *store, *md = start + (end - start) / 2;
-    double pivot;
-    while (1) {
-        pivot = md->x[idx];
-
-        swap(md, end - 1);
-        for (store = p = start; p < end; p++) {
-            if (p->x[idx] < pivot) {
-                if (p != store) swap(p, store);
-                store++;
-            }
-        }
-        swap(store, end - 1);
-
-        /* median has duplicate values */
-        if (store->x[idx] == md->x[idx]) return md;
-
-        if (store > md)
-            end = store;
-        else
-            start = store;
-    }
-}
-
-struct kdNode *kdTree::makeTree(struct kdNode *t, int len, int i) {
-    struct kdNode* n;
-    if (!len) return 0;
-    if ((n = findMedian(t, t + len, i))) {
-        i = (i + 1) % DIMENSION;
-        n->left = makeTree(t, n - t, i);
-        n->right = makeTree(n + 1, t + len - (n + 1), i);
-    }
-
-    return n;
-}
-
-struct kdNode *kdTree::buildTree(struct kdNode *t, int len, int i) {
-    struct kdNode * x, * root;
-    std::stack<kdNode *> s;
-
-    int start = 0;
-    int end = len;
-    int previous_start = start;
-    int previous_end = end;
-
-    x = findMedian(t +start, t + end, i);
-    int last_mid;;
-    root = x;
-
-    while (root != NULL) {
-        
-        
-        last_mid = x - t;
-        end = last_mid - previous_start;
-        start = previous_start;
-        i = (i + 1) % DIMENSION;
-
-        if (end >= start) {
-            x = findMedian(t + start, t +end, i);
-            root->left = x;
-            
-        }
-        else {
-            root->left = NULL;
-        }
-        
-        previous_start = start;
-        previous_end = end;
-        start = last_mid + 1;
-        end = previous_start + last_mid;
-
-        if (start<=end) {
-            root->right = x;
-            s.push(x);
-            s.push(t + start);
-            s.push(t + end);
-        } else {
-            break;
-        }
-
-        if (root->left != NULL) {
-            root = root->left;
-        }
-        else {
-            if (s.top() != NULL) {
-                previous_end = s.top() - t;
-                s.pop();
-            }
-            if (s.top() != NULL) {
-                previous_start = s.top() - t;
-                s.pop();
-            }
-            root= s.top();
-            s.pop();
-        }
-    }
-    return root;
-}
-
-std::vector<int> kdTree::rangeSearch(struct kdNode *root,
-    double searchPoint[DIMENSION]) {
-    double upperPoint[DIMENSION];
-
-    double lowerPoint[DIMENSION];
-
-    for (int i = 0; i < DIMENSION; i++) {
-        upperPoint[i] = searchPoint[i] + EPS;
-        lowerPoint[i] = searchPoint[i] - EPS;
-    }
-
-    std::vector<kdNode *> s;
-    kdNode *curr = root;
-
-    std::vector<int> points ={};
-
-    while (curr != NULL || s.empty() == false) {
-        while (curr != NULL) {
-            s.push_back(curr);
-            curr = curr->left;
-        }
-
-        curr = s.back();
-        s.pop_back();
-
-        if (curr->x[0] >= lowerPoint[0] && curr->x[0] <= upperPoint[0] &&
-            curr->x[1] >= lowerPoint[1] && curr->x[1] <= upperPoint[1]) {
-            points.push_back(curr->id);
-        }
-        curr = curr->right;
-    }
-
-    return points;
-}
-
-int main(int argc, char **argv) {
+int main(int argc, char ** argv) {
     char inputFname[500];
     if (argc != 2) {
         fprintf(stderr, "Please provide the dataset file path in the arguments\n");
@@ -176,7 +11,7 @@ int main(int argc, char **argv) {
     strcpy(inputFname, argv[1]);
     printf("Using dataset file %s\n", inputFname);
 
-    double *importedDataset =
+    double * importedDataset =
         (double *)malloc(sizeof(double) * DATASET_COUNT * DIMENSION);
 
     // Import data from dataset
@@ -191,7 +26,7 @@ int main(int argc, char **argv) {
         printf("Sample Data %f\n", importedDataset[i]);
     }
 
-    struct kdNode *wp =
+    struct kdNode * wp =
         (struct kdNode *)malloc(sizeof(struct kdNode) * DATASET_COUNT);
 
     for (int i = 0; i < DATASET_COUNT; i++) {
@@ -201,9 +36,12 @@ int main(int argc, char **argv) {
         }
     }
 
-    struct kdNode *root, *root2;
+    struct kdNode * root, * root2;
 
-    double searchPoint[DIMENSION] ={ 131.229813, 43.925762 };
+    double searchPoint[DIMENSION] ={
+        131.229813,
+        43.925762
+    };
 
     kdTree kd = kdTree();
 
@@ -215,22 +53,165 @@ int main(int argc, char **argv) {
     root = kd.buildTree(wp, DATASET_COUNT, 0);
     inOrderNoRecursion(root);
 
-    // std::vector<int> points = kd.rangeSearch(root, searchPoint);
+    std::vector < int > points = kd.rangeSearch(root, searchPoint);
 
-    // for (int i = 0; i < points.size(); i++) {
-    //     printf("%d\n", points[i]);
-    // }
+    for (int i = 0; i < points.size(); i++) {
+        printf("%d ", points[i]);
+    }
+
+    printf("\n");
 
     return 0;
 }
 
-void inOrderNoRecursion(struct kdNode *curr) {
-    std::stack<struct kdNode *> s;
+kdTree::kdTree() {}
+kdTree::~kdTree() {}
 
-    while (true)
-    {
-        while (curr != NULL)
-        {
+void kdTree::swap(struct kdNode * x, struct kdNode * y) {
+    double tmp[DIMENSION];
+    int id;
+
+    for (int i = 0; i < DIMENSION; i++) {
+        tmp[i] = x->x[i];
+    }
+    id = x->id;
+
+    for (int i = 0; i < DIMENSION; i++) {
+        x->x[i] = y->x[i];
+    }
+    id = x->id;
+    x->id = y->id;
+
+    for (int i = 0; i < DIMENSION; i++) {
+        y->x[i] = tmp[i];
+    }
+    y->id = id;
+}
+struct kdNode * kdTree::findMedian(struct kdNode * start, struct kdNode * end,
+    int idx) {
+    if (end <= start) return NULL;
+    if (end == start + 1) return start;
+
+    struct kdNode * p, * store, * md = start + (end - start) / 2;
+    double pivot;
+    while (1) {
+        pivot = md->x[idx];
+
+        swap(md, end - 1);
+        for (store = p = start; p < end; p++) {
+            if (p->x[idx] < pivot) {
+                if (p != store) swap(p, store);
+                store++;
+            }
+        }
+        swap(store, end - 1);
+
+        if (store->x[idx] == md->x[idx]) return md;
+
+        if (store > md)
+            end = store;
+        else
+            start = store;
+    }
+}
+
+struct kdNode * kdTree::makeTree(struct kdNode * t, int len, int i) {
+    struct kdNode * n;
+    if (!len) return 0;
+    if ((n = findMedian(t, t + len, i))) {
+        i = (i + 1) % DIMENSION;
+        n->left = makeTree(t, n - t, i);
+        n->right = makeTree(n + 1, t + len - (n + 1), i);
+    }
+
+    return n;
+}
+
+struct kdNode * kdTree::buildTree(struct kdNode * t, int len, int i) {
+    struct kdNode * x, * root;
+    std::stack < kdNode * > s;
+
+    int start = 0;
+    int end = len;
+
+    x = findMedian(t + start, t + end, i);
+    int last_mid;;
+    root = x;
+
+    while (root != NULL) {
+        last_mid = x - t;
+        end = last_mid - start;
+        i = (i + 1) % DIMENSION;
+        if (end >= start) {
+            x = findMedian(t + start, t + end, i);
+            root->left = x;
+        }
+        else {
+            root->left = NULL;
+        }
+        end = start + last_mid;
+        start = last_mid + 1;
+
+        if (start <= end) {
+            root->right = x;
+        }
+        else {
+            break;
+        }
+        if (root->left != NULL) {
+            root = root->left;
+        }
+    }
+    return root;
+}
+
+std::vector < int > kdTree::rangeSearch(struct kdNode * root,
+    double searchPoint[DIMENSION]) {
+    double upperPoint[DIMENSION];
+
+    double lowerPoint[DIMENSION];
+
+    for (int i = 0; i < DIMENSION; i++) {
+        upperPoint[i] = searchPoint[i] + EPS;
+        lowerPoint[i] = searchPoint[i] - EPS;
+    }
+
+    std::vector < int > points ={};
+
+    while (root) {
+        if (root->left == NULL) {
+            if (root->x[0] >= lowerPoint[0] && root->x[0] <= upperPoint[0] &&
+                root->x[1] >= lowerPoint[1] && root->x[1] <= upperPoint[1]) {
+                points.push_back(root->id);
+            }
+            root = root->right;
+        }
+        else {
+            struct kdNode * current = root->left;
+            while (current->right && current->right != root)
+                current = current->right;
+            if (current->right == root) {
+                current->right = NULL;
+                root = root->right;
+            }
+            else {
+                if (root->x[0] >= lowerPoint[0] && root->x[0] <= upperPoint[0] &&
+                    root->x[1] >= lowerPoint[1] && root->x[1] <= upperPoint[1]) {
+                    points.push_back(root->id);
+                }
+                current->right = root;
+                root = root->left;
+            }
+        }
+    }
+    return points;
+}
+
+void inOrderNoRecursion(struct kdNode * curr) {
+    std::stack < struct kdNode * > s;
+
+    while (true) {
+        while (curr != NULL) {
             s.push(curr);
             curr = curr->left;
         }
@@ -245,8 +226,9 @@ void inOrderNoRecursion(struct kdNode *curr) {
     printf("\n");
 }
 
-int ImportDataset(char const *fname, double *dataset) {
-    FILE *fp = fopen(fname, "r");
+int ImportDataset(char
+    const * fname, double * dataset) {
+    FILE * fp = fopen(fname, "r");
     if (!fp) {
         printf("Unable to open file\n");
         return (1);
@@ -255,7 +237,7 @@ int ImportDataset(char const *fname, double *dataset) {
     char buf[4096];
     unsigned long int cnt = 0;
     while (fgets(buf, 4096, fp) && cnt < DATASET_COUNT * DIMENSION) {
-        char *field = strtok(buf, ",");
+        char * field = strtok(buf, ",");
         long double tmp;
         sscanf(field, "%Lf", &tmp);
         dataset[cnt] = tmp;
--- kdtree/kdtree.h
+++ kdtree/kdtree.h
@@ -11,6 +11,9 @@
 #include <vector>
 
 
+#include<bits/stdc++.h>
+
+
 #define DATASET_COUNT 100
 #define DIMENSION 2
 #define EPS 1.5
@@ -27,7 +30,6 @@ private:
  public:
   __device__ __host__ kdTree();
   __device__ __host__ virtual ~kdTree();
-  __device__ __host__ inline  double dist(kdNode *a, kdNode *b);
   __device__ __host__ inline  void swap(kdNode *x, kdNode *y);
   __device__ __host__ kdNode *findMedian(kdNode *start, kdNode *end, int idx);
   kdNode *buildTree(kdNode *t, int len, int i);
@@ -39,3 +41,14 @@ private:
 
 void inOrderNoRecursion(struct kdNode *curr);
 int ImportDataset(char const *fname, double *dataset);
+
+#define gpuErrchk(ans) \
+  { gpuAssert((ans), __FILE__, __LINE__); }
+inline void gpuAssert(cudaError_t code, const char *file, int line,
+                      bool abort = true) {
+  if (code != cudaSuccess) {
+    fprintf(stderr, "GPUassert: %s %s %d\n", cudaGetErrorString(code), file,
+            line);
+    if (abort) exit(code);
+  }
+}
